c331cd5b3cc50e40ac032b3b448d9b881c535300
Aleksey Radevich <virtuain98@gmail.com>
2017-07-27 18:02:20 +0300

    Range question creator was added but not all bugs were fixed.

---
+++ InterviewPage/js/rangeQuestionCreator.js
@@ -0,0 +1,27 @@
+function createRangeQuestion() {
+    return {
+        create: function (question) {
+            var div = (document.createElement('div')); 
+            var $form = $('<form class="form"></form>');
+            var $startIndexValue = $('<label>').text(question.answers[1]);
+            var $endIndexValue = $('<label>').text(question.answers[2]);
+            var $input = $('<input>').attr({ 
+                type: 'range',  
+                name: question.id, 
+                id: 0,
+                value: question.answers[0],
+                min: question.answers[1],
+                max: question.answers[2],
+            });
+        debugger;
+            
+            $form.append($startIndexValue)
+                 .append($input)
+                 .append($endIndexValue);
+            $(div).append('<h4>' + question.text + '</h4>');  
+            $(div).append($form);
+
+            return div;
+        }
+    }
+}
